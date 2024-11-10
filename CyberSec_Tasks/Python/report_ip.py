455412c683ef72b47ee8ace9f46ca6e73748da72
Edgar-A23 <edgar.rodriguezp@uanl.edu.mx>
2024-11-10 15:07:00 -0600

    Update report_ip.py

--- CyberSec_Tasks/Python/report_ip.py
+++ CyberSec_Tasks/Python/report_ip.py
@@ -102,7 +102,7 @@ if open_ports:
                 py_menu()
     try:
         for port in ports:
-            name = "\\py-reports\\report_ip.txt"
+            name = "py-reports\\report_ip.txt"
             try:
                 url = "https://api.abuseipdb.com/api/v2/report"
                 parameters = {
